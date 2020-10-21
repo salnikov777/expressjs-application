6cce520cf2341c0912ad19f6aafaf60566c64a49
Salnikov Maksim <salnikov-m@yandex.ru>
2020-10-21 18:14:56 +0400

    MongoDB. Lesson 7 is done!

--- index.js
+++ index.js
@@ -5,19 +5,34 @@ const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cardRoutes = require('./routes/card');
-const mongoose  = require('mongoose')
+const mongoose = require('mongoose')
+const Handlebars = require('handlebars')
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const User = require('./models/user');
 
 const app = express();
 
 const hbs = exphbs.create({
     defaultLayout: 'main',
-    extname: 'hbs'
+    extname: 'hbs',
+    handlebars: allowInsecurePrototypeAccess(Handlebars)
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 
 app.set('views', 'views')
+
+app.use(async (req, res, next) => {
+
+    try{
+        const user = await User.findById('5f903b4680d8b018988f8ebb')
+        req.user = user;
+        next();
+    }catch (e) {
+        console.log(e);
+    }
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({extended: true}))
@@ -30,20 +45,34 @@ app.use('/card', cardRoutes);
 
 const PORT = process.env.PORT || 3000
 
-async function start(){
-    try{
+async function start() {
+    try {
         const urlDB = `mongodb+srv://maxim:maxim777@cluster0.5no97.mongodb.net/shop`
         await mongoose.connect(urlDB, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            useFindAndModify: false
         })
+        const candidate = await User.findOne();
+
+        if (!candidate) {
+            const user = new User({
+                email: 'salnikov-m@yandex.ru',
+                name: 'Max',
+                cart: {items: []}
+            })
+            await user.save();
+        }
+
+
         app.listen(PORT, () => {
             console.log('Server has been started!');
         })
-    }catch(e){
+    } catch (e) {
         console.log(e);
     }
 }
+
 start()
 
 
--- routes/courses.js
+++ routes/courses.js
@@ -4,7 +4,9 @@ const Course = require('../models/course');
 
 router.get('/', async (req, res) => {
 
-    const courses = await Course.getAll()
+    const courses = await Course.find()
+        .populate('userId', 'email name')
+        .select('price title img');
 
     res.render('courses', {
         title: 'Courses',
@@ -19,7 +21,7 @@ router.get('/:id/edit', async (req, res) => {
         return res.redirect('/')
     }
 
-    const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id)
 
     res.render('course-edit', {
         title: `Edit the course ${course.title}`,
@@ -28,19 +30,35 @@ router.get('/:id/edit', async (req, res) => {
 })
 
 router.post('/edit', async (req, res) => {
-    await Course.update(req.body)
+    const {id} = req.body
+    delete req.body.id;
+    await Course.findByIdAndUpdate(id, req.body)
     res.redirect('/courses')
 });
 
 router.get('/:id', async (req, res) => {
 
-    const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id)
 
     res.render('course', {
         layout: 'empty',
         title: `Курс ${course.title}`,
         course
     });
+})
+
+
+router.post('/remove', async (req, res) => {
+    try {
+        await Course.deleteOne({
+            _id: req.body.id
+        });
+        res.redirect('/courses')
+    } catch (e) {
+        console.log(e);
+    }
+
+
 })
 
 module.exports = router;
